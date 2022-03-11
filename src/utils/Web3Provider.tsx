1179fafdb53dc8cc10c0286d5d2c3e6d67b929b7
Marcin Gorzynski <marcin@gorzynski.me>
2022-03-11 13:20:53 +0100

    Web3Provider - implementation part 2

--- src/utils/Web3Provider.tsx
+++ src/utils/Web3Provider.tsx
@@ -1,7 +1,9 @@
 import {createContext, useCallback, useEffect, useState} from 'react';
 
-import { ethers } from "ethers";
+import {ethers, Signer} from "ethers";
 import { Provider } from "@ethersproject/abstract-provider";
+import { Network } from "@ethersproject/networks";
+
 import * as React from "react";
 
 const internalWeb3Context = createContext(undefined as unknown as Provider);
@@ -10,6 +12,8 @@ export const Web3Context = (props: {
     children: string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal | null | undefined;
     }) => {
     const [provider, setProvider] = useState(undefined as unknown as Provider)
+    const [signer, setSigner] = useState(undefined as unknown as Signer)
+    const [network, setNetwork] = useState(undefined as unknown as Network)
 
     const onNetworkChangeCallback = useCallback((oldNetwork, newNetwork) => {
         console.log("Network:", newNetwork);
@@ -21,20 +25,33 @@ export const Web3Context = (props: {
             // @ts-ignore
             window.ethereum.enable();
             // @ts-ignore
-            setProvider(new ethers.providers.Web3Provider(window.ethereum, "any"));
+            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+            const signer = provider.getSigner();
+            setProvider(provider);
+            setSigner(signer);
+
+            console.log("Provider:", provider);
+            console.log("Signer:", signer)
         }
     })
 
     useEffect(() => {
         async function fetchData() {
-            let network = await provider.getNetwork();
-            console.log("Network:", network);
+            if (provider === undefined) {
+                return;
+            }
 
+            provider.off("network");
+
+            let network = await provider.getNetwork();
+            setNetwork(network);
             provider.on("network", onNetworkChangeCallback);
+
+            console.log("Network:", network);
         }
 
         fetchData();
-    }, [])
+    }, [provider])
 
 
     return <internalWeb3Context.Provider value={provider}>
