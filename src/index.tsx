8e6da3e32fc73e625d4466887549a0f5ff36aded
Marcin Gorzynski <marcin@gorzynski.me>
2022-04-12 13:02:23 +0200

    Fix: problem with loading logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
+import './logrocket_init'
 import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
-
-import LogRocket from 'logrocket';
-// @ts-ignore
-import setupLogRocketReact from 'logrocket-react';
-
-LogRocket.init('h4vzcg/terminal');
-setupLogRocketReact(LogRocket);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/logrocket_init.ts
@@ -0,0 +1,6 @@
+import LogRocket from 'logrocket';
+// @ts-ignore
+import setupLogRocketReact from 'logrocket-react';
+
+LogRocket.init('h4vzcg/terminal');
+setupLogRocketReact(LogRocket);
